0f8076fc47f7e9206f163d161159dd3b5e3ff8be
Soumyajit Paul <soumyajit651@gmail.com>
2025-08-24 23:24:25 +0530

    Auth logic updated

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password } = req.body; 
   try {
     const hashed = await bcrypt.hash(password, 10);
     const user = await User.create({ name, email, password: hashed });
